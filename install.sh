bc041a8ba7e39b2c736deac6c156ab5fbd5e5865
Matthew Knox <mrkno@users.noreply.github.com>
2017-08-20 12:10:23 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@ set -e
 # run using:
 # curl -sL https://raw.githubusercontent.com/mrkno/InternetMonitor/master/install.sh | sudo -E bash -
 
-if [[ $EUID -ne 0 ]]; then
-	echo "This script can only be executed as root." 2>&1
-	exit 1
-fi
-
 git clone https://github.com/mrkno/InternetMonitor.git /opt/InternetMonitor
 chown -R $USER:$USER /opt/InternetMonitor
 cd /opt/InternetMonitor
