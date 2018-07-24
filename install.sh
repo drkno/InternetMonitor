6e72ffdda82511c79f7e3cd66609599cd686eed9
Matthew Knox <mrkno@users.noreply.github.com>
2018-07-24 23:46:36 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git clone https://github.com/mrkno/InternetMonitor.git /opt/InternetMonitor
 chown -R $USER:$USER /opt/InternetMonitor
 cd /opt/InternetMonitor
 npm install
-cp /opt/InternetMonitor/deploy/internet-monitor.service /etc/systemd/system/internet-monitor.service
+cp /opt/InternetMonitor/internet-monitor.service /etc/systemd/system/internet-monitor.service
 sed -i "s/REPLACEUSER/$USER/g" /etc/systemd/system/internet-monitor.service
 systemctl daemon-reload
 systemctl enable internet-monitor.service
